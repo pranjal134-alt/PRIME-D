3164c8e82e24827539e910a1d7c2f75134af2454
pranjal134-alt <sharmaharishankar700@gmail.com>
2026-04-02 13:00:57 +0530

    py

--- lec -4/pranjal.cpp
+++ lec -4/pranjal.cpp
@@ -1,6 +1,17 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main() {
-    int n;
-cin>> n;
-  
+int main()
+{
+    int n, k,gcd;
+    cin >> n >> k;
+    for (int i = 1; i <= n && i <= k; i++)
+    {
+        if (n % i == 0 && k % i == 0)
+        {
+            gcd = i;
+        }
+    }
+    cout << gcd;
+    return 0;
+   
+}
